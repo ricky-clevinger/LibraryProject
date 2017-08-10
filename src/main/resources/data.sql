3e36ac2cb8624adb28e1255381cd2309040e7457
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-10 16:00:35 -0400

    Role fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
 insert into app_user(id,name,username,password) values (1,'John Doe','admin','admin');
 insert into app_user(id,name,username,password) values (2,'Liza Elizabeth','user','user');
-insert into app_user(id,name,username,password) values (3,'Liza Elizabeth','lib','lib');
+insert into app_user(id,name,username,password) values (3,'Bill Jackson','lib','lib');
 
 insert into app_user_roles(app_user_id,roles) values (1,'ADMIN');
 insert into app_user_roles(app_user_id,roles) values (1,'USER');
+insert into app_user_roles(app_user_id,roles) values (1,'LIBRARIAN');
 insert into app_user_roles(app_user_id,roles) values (2,'USER');
 insert into app_user_roles(app_user_id,roles) values (3,'USER');
 insert into app_user_roles(app_user_id,roles) values (3,'LIBRARIAN');
