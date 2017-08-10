71490a14e64332bfcb557ab8ebd50dbfad3b8a1f
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-10 13:41:14 -0400

    Name changes

--- src/main/java/org/techforumist/jwt/LibraryProjectApplication.java
+++ src/main/java/org/techforumist/jwt/LibraryProjectApplication.java
@@ -3,10 +3,7 @@ package org.techforumist.jwt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Sarath Muraleedharan
- *
- */
+
 @SpringBootApplication
 public class JwtSpringBootSecurityAngularjsApplication {
 
